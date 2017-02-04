0f47002ca14d174e6eedaa6b77a98da3f4bb3797
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2017-02-03 23:20:25 -0500

    Check day of week

--- servers/secure.js
+++ servers/secure.js
@@ -1,38 +1,20 @@
-var url = require('url')
-var Serve = require('./serve')
-var Contact = require('./contact')
-var webhook = url.parse(process.env.SLACK_WEBHOOK_URL || require('../dev/slack_webhook_url'))
+var check_day = require('../lib/check_day')
 
 module.exports = route
 
 function route (req, res) {
-  req.url_parts = url.parse(req.url, true)
-  var serve = new Serve(req, res)
-
-  if (req.url_parts.path === '/') {
-    switch (req.method) {
-      case 'GET':
-        serve.microsite()
-        break
-
-      case 'POST':
-        var contact = new Contact(req, res, webhook)
-        var results
-        contact.process_form()
-          .then(results => serve.microsite(results))
-          .catch(e => {
-            console.log('An error ocurred while processing the contact form:',
-              e.message)
-            serve.error(e.status, e.message)
-          })
-        break
-
-      default:
-        return serve.error(405, 'Method not allowed')
-    }
-  } else if (req.url_parts.path.match(/^\/(?:images|stylesheets|javascript)/)) {
-    return serve.assets()
-  } else {
-    return serve.error(404, 'Document not found')
+  var right_now = new Date()
+  if (req.url !== '/') {
+    res.statusCode = 404
+    return res.end()
   }
+
+  res.setHeader('Content-Type', 'text/html')
+
+  check_day(right_now)
+  .then(() => {
+    res.end('YES') })
+  .catch(e => {
+    console.log(e)
+    res.end('NO') })
 }
