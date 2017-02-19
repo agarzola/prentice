bddb31e73de91b48467bbe2dab7699989551d03d
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2017-02-18 23:16:50 -0500

    Switch to http-only
    SSL handshake should be handled at the VM/proxy level

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var http = require('http')
-var https = require('https')
-var fs = require('fs')
 
-var unsecure = require('./servers/unsecure')
-var secure = require('./servers/secure')
+var request_handler = require('./lib/request_handler')
+var server = http.createServer(request_handler)
 
-var certs = {
-  key: fs.readFileSync('certs/key.pem'),
-  cert: fs.readFileSync('certs/cert.pem')
-}
-
-var unsecure_server = http.createServer(unsecure)
-var secure_server = https.createServer(certs, secure)
-
-unsecure_server.listen(8079)
-secure_server.listen(8080)
+server.listen(8080)
 console.log('Listening on port 8080…')
